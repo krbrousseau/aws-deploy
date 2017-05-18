6617d77105a9f41c595e9b0a7f6cfb943474ecaf
krbrousseau <krbrousseau@gmail.com>
2017-05-18 03:14:56 -0400

    broke up python script with comments

--- deploy.py
+++ deploy.py
@@ -3,8 +3,10 @@ import boto3
 import time
 import paramiko
 
+# load config
 config = json.load(open('configs/config.json','r'))
 
+# set up AWS session credentials
 session = boto3.session.Session(
   aws_access_key_id=config['session']['aws_access_key_id'],
   aws_secret_access_key=config['session']['aws_secret_access_key'],
@@ -31,6 +33,8 @@ time.sleep(90)
 print "continuing setup"
 for instances in deployed_instances:
   for instance in instances:
+    
+    # get instance IP
     instance = ec2.Instance(instance.instance_id)
     hostname = instance.public_ip_address
     key_name = instance.key_name
@@ -45,27 +49,34 @@ for instances in deployed_instances:
 	key_name = instance.key_name
       if hostname == None or key_name == None:
 	exit()
+
+    # set up ssh and ftp
     key_path = config['ssh']['key_path']+key_name+'.pem'
-    #print "ubuntu@"+str(hostname)+" "+str(key_path)
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     print "connecting to "+instance.instance_id+" at "+hostname
     ssh_client.connect(hostname, username="ubuntu", key_filename=key_path)
     ftp_client = ssh_client.open_sftp()
-    # push script file
+
+    # push setup script
     setup_script = config['ftp']['setup_script']
     setup_script_path = config['ftp']['script_path']+setup_script
     config_path = config['ftp']['config_path']
     print "uploading "+setup_script
     ftp_client.put(setup_script_path, "/home/ubuntu/"+setup_script)
     time.sleep(5)
+
+    # push config files
     for config_upload in config['ftp']['configs']:
       print "uploading "+config_upload
       ftp_client.put(config_path+config_upload, "/home/ubuntu/"+config_upload)
       time.sleep(5)
     ftp_client.close()
+
     print "waiting for instance"
     time.sleep(60)
+
+    # execute setup script and collect output
     print "executing "+setup_script
     stdin, stdout, stderr = ssh_client.exec_command('chmod +x '+setup_script+'; ./'+setup_script)
     script_output = ""
@@ -81,17 +92,14 @@ for instances in deployed_instances:
       stderr_buffer = stderr.readlines()
     print script_output
     print script_errors
+
+    # set up users
     for user in config['user_auth']:
       username = user['username']
       password = user['password']
       stdin, stdout, stderr = ssh_client.exec_command("sudo htpasswd -c -b /etc/nginx/.htpasswd "+username+" "+password)
-    stdin, stdout, stderr = ssh_client.exec_command("ps ax -o command | grep -c '^/usr/bin/java.*Elasticsearch'")
-    if int(stdout.readlines()[0])==1:
-      print "elasticsearch running"
-    else:
-      print "elasticsearch setup failed; exiting"
-      ssh_client.close()
-      exit()
+
+    # verify nginx is running
     stdin, stdout, stderr = ssh_client.exec_command("ps ax -o command | grep -c '^nginx'")
     if int(stdout.readlines()[0])>=1:
       print "nginx running"
@@ -99,5 +107,17 @@ for instances in deployed_instances:
       print "nginx setup failed; exiting"
       ssh_client.close()
       exit()
+
+    # verify elasticsearch is running
+    stdin, stdout, stderr = ssh_client.exec_command("ps ax -o command | grep -c '^/usr/bin/java.*Elasticsearch'")
+    if int(stdout.readlines()[0])==1:
+      print "elasticsearch running"
+    else:
+      print "elasticsearch setup failed; exiting"
+      ssh_client.close()
+      exit()
+
     ssh_client.close()
     print "elasticsearch available at "+hostname
+
+print "deploy complete"
